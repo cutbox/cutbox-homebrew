da1910ad906799eb8458f1d8ec20f086648c757e
jasonm23 <jasonm23@gmail.com>
2022-05-25 23:43:33 +0700

    1.4.13

--- Casks/cutbox.rb
+++ Casks/cutbox.rb
@@ -1,10 +1,11 @@
 cask "cutbox" do
-  version "1.4.12"
+  version "1.4.13"
 
-  sha256 "68ba878d6fa140d38d25fc97468129d0492b56f2b602b970e8daa992917b236e"
+  sha256 "18cabef007c7d074acef355bd0ce682be706a6040065d31bf5e20bf4b0222a21"
+
   url "https://github.com/cutbox/CutBox/releases/download/#{version}/CutBox.dmg"
   name "cutbox"
-  desc "clipboard history manager"
+  desc "Clipboard history manager"
   homepage "https://cutbox.github.io"
 
   app "CutBox.app"
