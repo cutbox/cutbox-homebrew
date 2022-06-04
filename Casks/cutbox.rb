391e37fa38e3428d4c69dc3aaa134ec4d089e942
jasonm23 <jasonm23@gmail.com>
2022-06-04 20:48:23 +0700

    Fix dmg sha

--- Casks/cutbox.rb
+++ Casks/cutbox.rb
@@ -1,6 +1,6 @@
 cask "cutbox" do
   version "1.4.19"
-  sha256 "a81cf50af26858b449806daced9eaf7860c1a1fb4766272d9ab1fa1c"
+  sha256 "e5dbe732ab0daddec883a4142de8dea648197201e8e512eaaa8955745ba6d44"
 
   url "https://github.com/cutbox/CutBox/releases/download/#{version}/CutBox.dmg"
   name "cutbox"
